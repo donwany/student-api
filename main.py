4f228caa51d515bc208c359f0834401302cba0ea
hardtanh <fcdjin@hardtanh.com>
2026-04-28 18:10:05 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from bson import ObjectId
 import os
 import uvicorn
