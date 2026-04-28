ea931a19e9ce99d4b1bfb73804b36bc1d3418ea5
hardtanh <fcdjin@hardtanh.com>
2026-04-28 18:09:34 -0500

    initial commit

--- main.py
+++ main.py
@@ -4,19 +4,39 @@ from typing import List
 from pymongo import MongoClient
 from bson import ObjectId
 import os
-
 import uvicorn
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
-# ---------------------------
-# Connect to local MongoDB
-# "mongodb://127.0.0.1:27017/?directConnection=true"
-# ---------------------------
 
+# ---------------------------
+# Connect to REMOTE MongoDB
+# 
+# ---------------------------
 MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
+# Create a new client and connect to the server
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+# ---------------------------
+# Connect to local MongoDB
+# 
+# ---------------------------
+
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://127.0.0.1:27017/?directConnection=true")
+# client = MongoClient(MONGO_URI)
+
+
 db = client["school_db"]
 students_collection = db["students"]
 
